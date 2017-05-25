364b8264cb257e6187557b2552150a60fc8832a5
Wijitha <wijithapaw@gmail.com>
2017-05-25 15:42:54 +0530

    Upgraded solution to VS2017

---
+++ DMS.Utills/ErrorHandlingMiddleware.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using System.Net;
+using Newtonsoft.Json;
+
+namespace DMS.Utills
+{
+    public class ErrorHandlingMiddleware
+    {
+        private readonly RequestDelegate next;
+
+        public ErrorHandlingMiddleware(RequestDelegate next)
+        {
+            this.next = next;
+        }
+
+        public async Task Invoke(HttpContext context /* other scoped dependencies */)
+        {
+            try
+            {
+                await next(context);
+            }
+            catch (Exception ex)
+            {
+                await HandleExceptionAsync(context, ex);
+            }
+        }
+
+        private static Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            var code = HttpStatusCode.InternalServerError; // 500 if unexpected
+
+
+            if (exception is NotImplementedException)
+                code = HttpStatusCode.NotImplemented;
+
+            /*
+            if (exception is MyNotFoundException) code = HttpStatusCode.NotFound;
+            else if (exception is MyUnauthorizedException) code = HttpStatusCode.Unauthorized;
+            else if (exception is MyException) code = HttpStatusCode.BadRequest;
+            */
+
+            var result = JsonConvert.SerializeObject(new { error = exception.Message });
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)code;
+            return context.Response.WriteAsync(result);
+        }
+    }
+}
